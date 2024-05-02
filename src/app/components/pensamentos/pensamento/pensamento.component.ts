cd50caaa7548f1e86708ae128d6bf93381867b81
unscrud <danieldevop@gmail.com>
2024-05-02 19:40:16 -0300

    larguraPensamento retorna a classe a ser usada de acordo com a quantidade de caracteres

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
   styleUrl: './pensamento.component.css'
 })
-export class PensamentoComponent {
+export class PensamentoComponent implements OnInit {
 
   @Input() pensamento = {
     conteudo: '',
     autoria: '',
     modelo: ''
+  }
+
+  constructor(){}
+
+  ngOnInit(): void{}
+
+  larguraPensamento(): string  {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
   }
 
 }
