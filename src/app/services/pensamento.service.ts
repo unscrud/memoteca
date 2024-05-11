2ff8d27de7fe699871314c9a01a2599bb48aa6a5
unscrud <danieldevop@gmail.com>
2024-05-11 16:02:48 -0300

    serviço pensamento

---
+++ src/app/services/pensamento.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pensamento } from '../components/pensamentos/pensamento/pensamento';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos';
+
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API);
+  }
+}
