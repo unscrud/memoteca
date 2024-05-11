1df9745a5deb5539eea6ccf2ddc36f5c8fbb46c2
unscrud <danieldevop@gmail.com>
2024-05-11 16:06:12 -0300

    carregando lista de pensamentos

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
+import { PensamentoService } from '../../../services/pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit{
   listaPensamentos: Pensamento[] = [];
+
+  constructor (private service: PensamentoService) {}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
 }
