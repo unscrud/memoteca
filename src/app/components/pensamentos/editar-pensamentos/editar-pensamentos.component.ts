2ef17c20dc8de3bf78f6707c30f0a2371e369e85
unscrud <danieldevop@gmail.com>
2024-05-15 21:31:12 -0300

    estrutura básica do componente

--- src/app/components/pensamentos/editar-pensamentos/editar-pensamentos.component.ts
+++ src/app/components/pensamentos/editar-pensamentos/editar-pensamentos.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
 
 @Component({
   selector: 'app-editar-pensamentos',
   templateUrl: './editar-pensamentos.component.html',
   styleUrl: './editar-pensamentos.component.css'
 })
-export class EditarPensamentosComponent {
+export class EditarPensamentosComponent implements OnInit {
+  pensamento: Pensamento = {
+    id: '',
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
 
+  constructor (){}
+
+  ngOnInit(): void {
+
+  }
+
+  editarPensamento(){}
+
+  cancelar(){}
 }
