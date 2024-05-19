1255f637608602986a7597d1d843cf6337e0c254
unscrud <danieldevop@gmail.com>
2024-05-19 12:44:39 -0300

    adicionando validações de conteúdo apenas com espaços e de tamanho mínimo

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -22,14 +22,21 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['', [Validators.required]],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
       modelo: ['modelo1'],
     });
   }
 
   criarPensamento(){
-    console.log(this.formulario)
+    console.log(this.formulario.status)
     if (this.formulario.valid){
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento']);
