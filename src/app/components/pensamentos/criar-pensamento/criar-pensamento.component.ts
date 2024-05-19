2eb194da15a771dfab901da3a0443a449ed11c7c
unscrud <danieldevop@gmail.com>
2024-05-19 12:37:38 -0300

    adição de validadores ao form  e apenas criando se for válido

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
 import { PensamentoService } from '../../../services/pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -22,16 +22,19 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: [''],
-      autoria: [''],
-      modelo: ['modelo1']
-    })
+      conteudo: ['', [Validators.required]],
+      autoria: ['', [Validators.required]],
+      modelo: ['modelo1'],
+    });
   }
 
   criarPensamento(){
-    this.service.criar(this.formulario.value).subscribe(()=>{
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario)
+    if (this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
   }
 
   cancelar(){
