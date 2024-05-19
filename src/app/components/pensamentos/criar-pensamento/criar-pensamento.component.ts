193322394109d1f07adfcd8df8b77158158a057c
unscrud <danieldevop@gmail.com>
2024-05-19 11:54:17 -0300

    criação do formulario como um formgroup e usando um builder para definir os valores

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,24 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
 import { PensamentoService } from '../../../services/pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
-export class CriarPensamentoComponent {
+
+export class CriarPensamentoComponent implements OnInit {
   pensamento: Pensamento = {
     conteudo: '',
     autoria: '',
     modelo: ''
   }
 
+  formulario!: FormGroup;
+
   constructor(
     private service: PensamentoService,
+    private formBuilder: FormBuilder,
     private router: Router
   ){}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: [''],
+      modelo: ['modelo1']
+    })
+  }
 
   criarPensamento(){
     this.service.criar(this.pensamento).subscribe(()=>{
