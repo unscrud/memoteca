116be9cb01c79b51d0feef23a9aaa238f9d71a13
unscrud <danieldevop@gmail.com>
2024-08-18 16:43:11 -0300

    observando a lista de pensamentos favoritos

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -14,6 +14,7 @@ export class ListarPensamentoComponent implements OnInit {
   haMaisPensamentos: boolean = true;
   filtro: string = '';
   favoritos: boolean = false;
+  listaFavoritos : Pensamento[] = [];
 
   constructor(private service: PensamentoService) {}
 
@@ -56,6 +57,7 @@ export class ListarPensamentoComponent implements OnInit {
       .listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe((pensamentosFavoritos: Pensamento[]) => {
         this.listaPensamentos = pensamentosFavoritos;
+        this.listaFavoritos = pensamentosFavoritos;
       });
   }
 }
