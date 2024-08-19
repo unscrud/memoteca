9db41d3e76e2700dbd720dc85cd88430b740352d
unscrud <danieldevop@gmail.com>
2024-08-19 17:10:40 -0300

    recarregar componente sem reload da página

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
 import { PensamentoService } from '../../../services/pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -16,7 +17,10 @@ export class ListarPensamentoComponent implements OnInit {
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) {}
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.service
@@ -50,7 +54,12 @@ export class ListarPensamentoComponent implements OnInit {
   }
 
   recarregarComponente() {
-    location.reload();
+    this.favoritos = false;
+    this.paginaAtual = 1;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
   }
 
   listarFavoritos() {
